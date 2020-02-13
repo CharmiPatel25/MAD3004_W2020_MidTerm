fd64459baa89d195e65ccaeaea8f0e54d1174804
charmi patel <C0768448@mylambton.ca>
2020-02-13 18:43:02 -0500

    [CP] Create Customer Class

---
+++ C0768448_W2020_MID_TERM_MAD3004/C0768448_W2020_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,21 @@
+//
+//  Customer.swift
+//  C0768448_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Customer
+{
+    var customerID:Int
+    var firstName:String
+    var lastName:String
+    var fullName:String
+    {
+        return " \(firstName) \(lastName)"
+    }
+    var emailID:String
+    
+}
